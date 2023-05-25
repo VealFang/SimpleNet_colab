cd6cbf4667c17708ada0e8d2a5a616ea7e2ee0aa
Fang <50687763+VealFang@users.noreply.github.com>
2023-05-25 16:37:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,6 +12,72 @@ import tqdm
 LOGGER = logging.getLogger(__name__)
 
 
+def denormalization(x):
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    x = (((x.transpose(1, 2, 0) * std) + mean) * 255.).astype(np.uint8)
+
+    return x
+
+
+def plot_fig(test_img, scores, gts_list, masks, save_dir, class_name):
+    # plot_fig(test_data, scores, masks_gt, norm_segmentations, self.plots_dir, self.dataset_name)
+    # img_paths, scores, masks_gt, masks, self.plots_dir, self.dataset_name
+    # img_paths = []
+    # for data in test_data:
+    #     img_paths.extend(data['image'])
+    num = len(scores)
+    print('scores: ', scores)
+    print(num)
+    vmax = scores.max() * 255.
+    vmin = scores.min() * 255.
+    for i in range(num):
+        img = test_img[i]
+        img = denormalization(img)
+        gts = np.asarray(gts_list)
+        gt = gts[i].transpose(1, 2, 0).squeeze()
+        heat_map = scores[i] * 255
+        mask = masks[i]
+        mask *= 255
+        # vis_img = mark_boundaries(img, mask, color=(1, 0, 0), mode='thick')
+        fig_img, ax_img = plt.subplots(1, 4, figsize=(12, 3))
+        fig_img.subplots_adjust(right=0.9)
+        norm = matplotlib.colors.Normalize(vmin=vmin, vmax=vmax)
+        for ax_i in ax_img:
+            ax_i.axes.xaxis.set_visible(False)
+            ax_i.axes.yaxis.set_visible(False)
+        ax_img[0].imshow(img)
+        ax_img[0].title.set_text('Image')
+        ax_img[1].imshow(gt, cmap='gray')
+        ax_img[1].title.set_text('GroundTruth')
+        ax = ax_img[2].imshow(heat_map, cmap='jet', norm=norm)
+        ax_img[2].imshow(img, cmap='gray', interpolation='none')
+        ax_img[2].imshow(heat_map, cmap='jet', alpha=0.5, interpolation='none')
+        ax_img[2].title.set_text('Predicted heat map')
+        ax_img[3].imshow(mask, cmap='gray')
+        ax_img[3].title.set_text('Predicted mask')
+        # ax_img[4].imshow(vis_img)
+        # ax_img[4].title.set_text('Segmentation result')
+        left = 0.92
+        bottom = 0.15
+        width = 0.015
+        height = 1 - 2 * bottom
+        rect = [left, bottom, width, height]
+        cbar_ax = fig_img.add_axes(rect)
+        cb = plt.colorbar(ax, shrink=0.6, cax=cbar_ax, fraction=0.046)
+        cb.ax.tick_params(labelsize=8)
+        font = {
+            'family': 'serif',
+            'color': 'black',
+            'weight': 'normal',
+            'size': 8,
+        }
+        cb.set_label('Anomaly Score', fontdict=font)
+
+        fig_img.savefig(os.path.join(save_dir, class_name + '_{}'.format(i)), dpi=100)
+        plt.close()
+        
+        
 def plot_segmentation_images(
     savefolder,
     image_paths,
